2f2adabc05c39ad0d56d2243f02991a8cc5f36a3
Antoni Zyla <zyla.antoni@gmail.com>
2023-10-31 09:37:50 +0000

    basic doubly linked list

--- src/main.c
+++ src/main.c
@@ -3,9 +3,26 @@
 #include <unistd.h>
 
 #include "lexer.h"
-
+#include "ds/list.h"
+ 
 int main() {
-  char *processed = processFile("src/lexer.h");
-  free(processed);
-  return 0;
+//  char *processed = processFile("src/lexer.h");
+//  free(processed);
+
+    // test the linked list 
+    list_t *list_t = listInitialise();
+    int a = 2;
+    int b = 2;
+    int c = 3;
+    appendItem(list, &a);
+    appendItem(list, &b);
+    appendItem(list, &c);
+    printList(list);
+    removeTail(list);
+    printList(list);
+    prependItem(list, &c);
+    printList(list);
+    freeList(list);
+
+    return 0;
 }
